f34613d59cd7c384b1ad12c604b48859f902066d
Badri <programmingwithbadri@outlook.com>
2022-12-24 14:26:14 +0530

    Consume remote modules

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,16 @@
 import faker from 'faker';
 
-const message = `<div>You have${faker.random.number()} items in your cart</div>`;
+const mount = (el) => {
+    const message = `<div>You have${faker.random.number()} items in your cart</div>`;
+    el.innerHTML = message;
+};
 
-document.querySelector('#dev-cart').innerHTML = message;
+// Test microfrontend in isolation
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#dev-cart');
+    if (el) {
+        mount(el);
+    }
+}
+
+export { mount };
--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,3 +1,6 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
+import { mount as mountProducts } from 'products/ProductsIndex';
+import { mount as mountCart } from 'cart/CartShow';
 console.log('container');
+
+mountProducts(document.querySelector('#my-products'));
+mountCart(document.querySelector('#my-cart'));
